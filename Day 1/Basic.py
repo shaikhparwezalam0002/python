a954fcbcdd8d9e9cd0e70d527d9a253352adf307
Shaikh Parwez Alam <79353520+shaikhparwezalam0002@users.noreply.github.com>
2025-09-29 20:46:14 +0000

    commit

--- Day 1/Basic.py
+++ Day 1/Basic.py
@@ -1,5 +1,9 @@
 #print(2**3) #Exponential operator
 
-x = input()
-y = input()
-print(x+5) #can only concatenate str (not "int") to str
+# x = input()
+# y = input()
+# print(x+5) #can only concatenate str (not "int") to str
+
+
+x = int(input())
+print(x)
