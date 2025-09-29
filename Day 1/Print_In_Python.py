307825baa0a9bc1e49920494569806b642744d35
Shaikh Parwez Alam <79353520+shaikhparwezalam0002@users.noreply.github.com>
2025-09-29 21:20:35 +0000

    Day 1

---
+++ Day 1/Print_In_Python.py
@@ -0,0 +1,13 @@
+# Print with separator
+# Print with space
+# Print without newline at the end
+# Print without space
+########### Write your code above ###############
+a = input()
+b= input()
+c=input()
+print(a+' '+b,end="")
+print(a+" "+b)
+print(a+b)
+print(a+c+b)
+########### Write your code below ###############
---
+++ Day 1/StartCoding.py
@@ -0,0 +1,5 @@
+# When learning a new language, we first learn to output some message. Here we will start with the famous "Hello World" message.
+# Complete the python code to print "Hello World".
+
+#code here
+print("Hello World",end="")
